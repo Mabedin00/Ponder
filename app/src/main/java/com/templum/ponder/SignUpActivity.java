5e0ceac58a891a8c92c25db621ff5b71954597cc
Mohidul Abedin <mabedin00@stuy.edu>
2018-11-23 20:44:06 -0500

    Initial Commit

---
+++ app/src/main/java/com/templum/ponder/SignUpActivity.java
@@ -0,0 +1,114 @@
+package com.templum.ponder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity{ // implements View.OnClickListener{
+    private EditText emailField;
+    private EditText passwordField;
+    private EditText passwordField1;
+    private EditText usernameField;
+    private Button signUpBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        emailField = (EditText) findViewById(R.id.emailField);
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        passwordField1 = (EditText) findViewById(R.id.passwordField1);
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+
+            }
+        });
+
+
+
+//        findViewById(R.id.signUpBtn).setOnClickListener(this);
+
+    }
+    public void registerUser(){
+        String email = emailField.getText().toString().trim();
+        String username = usernameField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+        String password1 = passwordField1.getText().toString().trim();
+
+        if (email.isEmpty()){
+            emailField.setError("Email is Required");
+            emailField.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailField.setError("Email needs to be valid");
+            emailField.requestFocus();
+            return;
+        }
+        if (username.isEmpty()){
+            usernameField.setError("Username is Required");
+            usernameField.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            passwordField.setError("Password is Required");
+            passwordField.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6){
+            passwordField.setError("Password needs to have at least 6 characters");
+            passwordField.requestFocus();
+            return;
+        }
+
+        if (!password1.equals(password)){
+            passwordField1.setError("Passwords must match");
+            passwordField1.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "You are Registered!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "REEEEEEEEEE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch(v.getId()){
+//            case R.id.signUpBtn:
+//                registerUser();
+//                break;
+//
+//
+//        }
+//
+//    }
+}
---
+++ app/src/main/java/com/templum/ponder/StartUpActivity.java
@@ -0,0 +1,42 @@
+package com.templum.ponder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+
+        Button logInPgBtn = (Button) findViewById(R.id.logInPgBtn);
+
+        logInPgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogInActivity();
+            }
+        });
+
+        Button signUpPgBtn = (Button) findViewById(R.id.signUpPgBtn);
+        signUpPgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
+
+
+    }
+    public void openLogInActivity(){
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+    public void openSignUpActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
